df840bc941f1ebdfc2beb83f09fbb30ef5499a89
Yure Pablo <48031838+yp1plus@users.noreply.github.com>
2019-08-30 15:38:40 -0300

    Update Catalan.c

--- Catalan.c
+++ Catalan.c
@@ -1,11 +1,11 @@
-// Vinicius Silva Fernandes Kuhlmann 11215751
+// Done by Vinicius Silva Fernandes Kuhlmann
 
 // Como o programa usa scanf e printf, ele precisa da biblioteca stdio.h
 #include <stdio.h>
 
 // A funcao abaixo retorna um valor inteiro, calculado com base na funcao dos numeros de catalan aplicada ao valor inteiro n.
 int catalan(int n) {
-	// Se n for igual a 0, a funcao retorna sem fazer nada.
+	// Se n for igual a 0, a funcao retorna 1 sem fazer nada.
     if (n == 0)
         return 1;
 	// Se n nao for igual a 0, a funcao retorna ( (4*n - 2) * catalan(n-1) / (n+1) ). Como a funcao e recursiva, catalan(n-1) = ( (4*(n-1) - 2) * catalan(n-2) / (n) ), e assim por diante, ate que n seja 0.
