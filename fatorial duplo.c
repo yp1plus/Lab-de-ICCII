0cea380c43acc65bc5260be3aced2eef4cb6c1a2
Yure Pablo <48031838+yp1plus@users.noreply.github.com>
2019-08-30 15:42:22 -0300

    Update fatorial duplo.c

--- fatorial duplo.c
+++ fatorial duplo.c
@@ -1,4 +1,4 @@
-// Vinicius Silva Fernandes Kuhlmann 11215751
+// Done by Vinicius Silva Fernandes Kuhlmann
 
 // Como o programa usa scanf e printf, ele precisa da biblioteca stdio.h
 #include <stdio.h>
@@ -8,7 +8,7 @@ int fat_d(int n) {
 	// Se n for par, a funcao tranforma o valor em impar.
     if (!n%2)
         n--;
-	// Se n for igual a 1, a funcao retorna sem fazer nada.
+	// Se n for igual a 1, a funcao retorna 1 sem fazer nada.
     if (n==1)
         return 1;
 	// // Se n nao for igual a 1, a funcao retorna n * fat_d(n-2). Como a funcao e recursiva, fat_d(n-2) = (n-2) * fat_d(n-4), e assim por diante, ate que n seja 1.
